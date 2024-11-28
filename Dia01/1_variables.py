e65af0d28a5e045ca4ac27587a593668de4bae63
carlos <cesarfernado912@gmail.com>
2024-11-27 21:24:28 -0500

    semana 1 actualizado

---
+++ Dia01/1_variables.py
@@ -0,0 +1,15 @@
+nombre = 'WILDER'
+direccion = "LAS LECHUZAS"
+
+enunciado = '''hola 
+como estas
+        Te estraño
+        espero verte pronto,
+        saludos'''
+
+otro_enunciado = """hola 
+como estas
+        Te estraño
+        espero verte pronto,
+        saludos"""
+
